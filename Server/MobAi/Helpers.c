47f22fd907514281888f411b6e86f04b2b645eb4
User75670 <u007549@gmail.com>
2026-06-02 09:52:58 +0000

    minor change

--- Server/MobAi/Helpers.c
+++ Server/MobAi/Helpers.c
@@ -237,10 +237,10 @@ void tick_return_to_higher_zone(EntityIdx entity,
     if (mob->rarity < rr_rarity_id_ultimate)
         return;                                           //  e    l
     int whichRarity = mob->rarity == rr_rarity_id_ultimate 
-    ? 24 // e
+    ? 40 // m
     : mob->rarity == rr_rarity_id_eternal 
-    ? 32 // l
-    : 40; // m
+    ? 48 // x
+    : 56; // a
     struct rr_component_arena *arena =
         rr_simulation_get_arena(simulation, physical->arena);
     int32_t grid_x = rr_fclamp(physical->x / arena->maze->grid_size,
